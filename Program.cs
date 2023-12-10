f1ae0bdd8d2457d34f126d6af24141bb7992027c
Nader Kafe <z1949619@students.niu.edu>
2023-12-10 08:25:12 -0600

    Final commit for submition

--- Program.cs
+++ Program.cs
@@ -1,137 +1,65 @@
-﻿using System;
+﻿//Nader Kafe            Z1949619                Code:- september 18th, 2023
+//**********************************************************************//
+using System; // Import the System namespace, which contains fundamental types and base classes
 using System.Diagnostics;
-
 class Program
 {
     static void Main()
     {
-        int n = 800000;
-        double[,] numbers;
-        Stopwatch timer = new Stopwatch();
+        int n = 800000; // Number of random numbers to generate
+        double[,] numbers; // Declare a 2D array to store random numbers
+        Stopwatch timer = new Stopwatch(); // Stopwatch to measure execution time
+        
+        numbers = GenRandomNumbers(n);      // Generate random numbers and store them in the 'numbers' array
 
-        // Question 1: Comparing the speed of adding floats vs. doubles
-        Console.WriteLine("1:- Comparing the speed of adding floats vs. doubles");
+        timer.Start();                            // Start measuring time
+        AddNumbers(numbers, n);       // Perform addition operation on the 'numbers' array
+        timer.Stop();                             // Stop measuring time
 
-        float[,] floatNumbers = GenRandomFloatNumbers(n);
-        double[,] doubleNumbers = GenRandomNumbers(n);
+        // Display results for addition
+        Console.WriteLine("Additions");
+        Console.WriteLine("Elapsed time = " +
+            timer.ElapsedMilliseconds + " ms " +
+            timer.ElapsedTicks + " ticks\n");
 
-        timer.Start();
-        AddNumbers(floatNumbers, n);
-        timer.Stop();
-        Console.WriteLine("Adding floats took: " + timer.ElapsedMilliseconds + " ms " + timer.ElapsedTicks + " ticks\n");
+        float addTicks = (float)timer.ElapsedTicks;           // Store elapsed ticks for addition
+        
+        timer.Restart();                                      // Restart the timer for the multiplication operation
+        MultiplyNumbers(numbers, n);              // Perform multiplication operation on the 'numbers' array
+        timer.Stop();                                         // Stop measuring time
+        Console.WriteLine("Multiplications");          // Display results for multiplication
+        Console.WriteLine("Elapsed time = " +
+            timer.ElapsedMilliseconds + " ms " +
+            timer.ElapsedTicks + " ticks\n");
 
-        // Reset arrays
-        floatNumbers = GenRandomFloatNumbers(n);
-        doubleNumbers = GenRandomNumbers(n);
+        float multTicks = (float)timer.ElapsedTicks; // Store elapsed ticks for multiplication
 
-        timer.Reset();
-        timer.Start();
-        AddNumbers(doubleNumbers, n);
-        timer.Stop();
-        Console.WriteLine("Adding doubles took: " + timer.ElapsedMilliseconds + " ms " + timer.ElapsedTicks + " ticks\n");
-
-        // Question 2: Comparing the speed of computing squares and square roots
-        Console.WriteLine("\n2:- Comparing the speed of computing squares and square roots");
-
-        numbers = GenRandomNumbers(n);
-
-        // Compute squares using direct multiplication
-        timer.Reset();
-        timer.Start();
-        ComputeSquaresDirectly(numbers, n);
-        timer.Stop();
-        Console.WriteLine("Computing squares directly took: " + timer.ElapsedMilliseconds + " ms " + timer.ElapsedTicks + " ticks\n");
-
-        // Reset array
-        numbers = GenRandomNumbers(n);
-
-        // Compute squares using Math.Pow function
-        timer.Reset();
-        timer.Start();
-        ComputeSquaresWithMathPow(numbers, n);
-        timer.Stop();
-        Console.WriteLine("Computing squares with Math.Pow took: " + timer.ElapsedMilliseconds + " ms " + timer.ElapsedTicks + " ticks\n");
-
-        // Reset array
-        numbers = GenRandomNumbers(n);
-
-        // Compute square roots using Math.Sqrt function
-        timer.Reset();
-        timer.Start();
-        ComputeSquareRootsWithMathSqrt(numbers, n);
-        timer.Stop();
-        Console.WriteLine("Computing square roots with Math.Sqrt took: " + timer.ElapsedMilliseconds + " ms " + timer.ElapsedTicks + " ticks\n");
+        // Calculate and display the ratio of addition time to multiplication time
+        Console.WriteLine("Ratio = " + addTicks / multTicks);
     }
-
-    static double[,] GenRandomNumbers(int count)
+    static double[,] GenRandomNumbers(int count)    // Function to generate an array of random double numbers
     {
-        Random rand = new Random();
-        double[,] num = new double[count, 3];
-        for (int i = 0; i < count; ++i)
+        Random rand = new Random();     // instantiate (Create a new instance) of the Random class to generate random numbers
+        double[,] num = new double[count,3]; // Declare(make) an array 'num' to store double values with the specified 'count'
+        for (int i = 0; i < count; ++i) // Iterate through the 'num' array and assign random double values
         {
-            num[i, 0] = 10000.0 * rand.NextDouble();
-            num[i, 1] = 10000.0 * rand.NextDouble();
+            num[i,0] = 10000.0 * rand.NextDouble(); // Generate a random double value between 0 and 1000 and store it in the array
+            num[i,1] = 10000.0 * rand.NextDouble(); // Generate random number between 0 and 10000  //**** or ****// fill in elements of array
         }
-        return num;
-    }
-
-    static float[,] GenRandomFloatNumbers(int count)
-    {
-        Random rand = new Random();
-        float[,] num = new float[count, 3];
-        for (int i = 0; i < count; ++i)
-        {
-            num[i, 0] = (float)(10000.0 * rand.NextDouble());
-            num[i, 1] = (float)(10000.0 * rand.NextDouble());
-        }
-        return num;
-    }
-
-    static void AddNumbers(float[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = nums[i, 0] + nums[i, 1];
-        }
-    }
-
+        return num;  // Return the array of double numbers
+    }            // returns reference to array
+    // Function that adds numbers in the supplied 2D array
     static void AddNumbers(double[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = nums[i, 0] + nums[i, 1];
+    {   int i; // Declare a variable 'i' for the loop
+        for (i = 0; i < count; ++i){  // Iterate through the rows of the 'nums' array
+        nums[i, 2] = nums[i, 0] + nums[i, 1];  // Add the values in the first and second columns of each row and store the result in the third column
         }
     }
-
+// Function that multiplies numbers in the supplied 2D array
     static void MultiplyNumbers(double[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = nums[i, 0] * nums[i, 1];
-        }
-    }
-
-    static void ComputeSquaresDirectly(double[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = nums[i, 0] * nums[i, 0];
-        }
-    }
-
-    static void ComputeSquaresWithMathPow(double[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = Math.Pow(nums[i, 0], 2);
-        }
-    }
-
-    static void ComputeSquareRootsWithMathSqrt(double[,] nums, int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            nums[i, 2] = Math.Sqrt(nums[i, 0]);
+    {   int i; // Declare a variable 'i' for the loop
+        for (i = 0; i < count; ++i){// Iterate through the rows of the 'nums' array
+        nums[i, 2] = nums[i, 0] * nums[i, 1];   // Multiply the values in the first and second columns of each row and store the result in the third column
         }
     }
 }
